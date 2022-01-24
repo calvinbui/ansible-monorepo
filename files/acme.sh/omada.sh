e741e7d5a1f511621c988410b4193952dd38dd91
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-01-24 20:50:57 +1100

    acme: delete unifi and add omada

---
+++ files/acme.sh/omada.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CERT=$1
+KEY=$2
+
+mkdir -p /apps/omada/cert/
+
+cp "$CERT" /apps/omada/cert/tls.crt
+cp "$KEY" /apps/omada/cert/tls.key
+
+# Restart Omada controller
+docker restart omada
