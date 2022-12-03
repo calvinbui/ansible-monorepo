453ea6a6dc13bcceb6473a7ee31cc0f2049e4821
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-12-03 12:47:58 +1100

    ci: allow specifying playbooks (space separated)

--- .drone/ansible-playbook.sh
+++ .drone/ansible-playbook.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
-mapfile -t playbooks_updated < <(git show --name-only HEAD | grep -E "^(\w|-|_)+.y*ml")
+if [[ -z $playbooks ]]; then
+  mapfile -t playbooks < <(git show --name-only HEAD | grep -E "^(\w|-|_)+.y*ml")
+else
+  IFS=" " read -r -a playbooks <<< "${playbooks[@]}"
+fi
 
-if [[ "${#playbooks_updated[@]}" -eq 0 ]]; then
+if [[ "${#playbooks[@]}" -eq 0 ]]; then
   echo "--- No playbooks changed"
   exit 0
 fi
 
-echo "--- Put vault key"
-echo "$ANSIBLE_VAULT_PASSWORD" > ~/.vault_pass.txt
+if [ ! -f ~/.vault_pass.txt ]; then
+  echo "--- Put vault key"
+  echo "$ANSIBLE_VAULT_PASSWORD" > ~/.vault_pass.txt
+fi
 
-# skip_playbooks=(
-#   "drone.yml"
-#   "nvidia.yml"
-#   "docker.yml"
-# )
-
-for playbook in "${playbooks_updated[@]}"; do
-  # shellcheck disable=SC2076
-  # if [[ " ${skip_playbooks[*]} " =~ " ${playbook} " ]]; then
-  #   continue
-  # fi
+for playbook in "${playbooks[@]}"; do
+  # if provided without the extension
+  if [[ $playbook != *.yml ]]; then
+    playbook="${playbook}.yml"
+  fi
 
   echo "--- $playbook"
   ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook "$playbook"
