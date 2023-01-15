8e4cc9f1ae029692f3ad5b69091fd899131b49e6
Calvin Bui <calvinbui@noreply.gitea.bui.services>
2023-01-15 13:35:07 +1100

    crowdsec (#739)
    Co-authored-by: Calvin Bui <3604363+calvinbui@users.noreply.github.com>
    Reviewed-on: https://gitea.bui.services/calvinbui/ansible-monorepo/pulls/739

--- files/acme.sh/omada.sh
+++ files/acme.sh/omada.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 CERT=$1
 KEY=$2
 
-mkdir -p /apps/omada/cert/
+mkdir -p "{{ common_directory }}/omada/cert/"
 
-cp "$CERT" /apps/omada/cert/tls.crt
-cp "$KEY" /apps/omada/cert/tls.key
+cp "$CERT" "{{ common_directory }}/omada/cert/tls.crt"
+cp "$KEY" "{{ common_directory }}/omada/cert/tls.key"
 
 # Restart Omada controller
 docker restart omada
