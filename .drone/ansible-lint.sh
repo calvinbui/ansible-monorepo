53dafab965569ae1300e7644b4879d50c5953fea
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2023-03-27 00:44:19 +1100

    ci: selective lint

--- .drone/ansible-lint.sh
+++ .drone/ansible-lint.sh
@@ -2,8 +2,30 @@
 
 set -euo pipefail
 
+if [[ -z $playbooks ]]; then
+  mapfile -t allplaybooks < <(git show --name-only --diff-filter=ACMRTU HEAD | grep -E "^(\w|-|_)+.y*ml")
+else
+  IFS="," read -r -a playbooks <<< "${playbooks[@]}"
+  allplaybooks=( )
+  for playbook in "${playbooks[@]}"; do
+    if [[ $playbook != *.yml ]]; then
+      playbook="${playbook}.yml"
+    fi
+
+    allplaybooks+=( "$playbook" )
+  done
+fi
+
+if [[ "${#allplaybooks[@]}" -eq 0 ]]; then
+  echo "--- No playbooks changed"
+  exit 0
+fi
+
+
 echo "--- Put vault key"
 echo "$ANSIBLE_VAULT_PASSWORD" > ~/.vault_pass.txt
 
 echo "--- ansible-lint"
-ansible-lint ./*.y*l
+# shellcheck disable=SC2068
+# i want it to split
+ansible-lint ${allplaybooks[@]}
