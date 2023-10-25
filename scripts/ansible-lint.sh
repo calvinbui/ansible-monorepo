65c200f8535940b9b1396864a00ee2ce4d845b9f
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2023-10-26 00:03:14 +1100

    ci: remove manual input

--- scripts/ansible-lint.sh
+++ scripts/ansible-lint.sh
@@ -2,27 +2,14 @@
 
 set -eo pipefail
 
-if [[ -z $playbooks ]]; then
-  mapfile -t allplaybooks < <(git diff --name-only -r HEAD^1 HEAD | grep -E "^(\w|-|_)+.y*ml")
-else
-  IFS="," read -r -a playbooks <<< "${playbooks[@]}"
-  allplaybooks=( )
-  for playbook in "${playbooks[@]}"; do
-    if [[ $playbook != *.yml ]]; then
-      playbook="${playbook}.yml"
-    fi
-
-    allplaybooks+=( "$playbook" )
-  done
-fi
+mapfile -t allplaybooks < <(git diff --name-only -r HEAD^1 HEAD | grep -E "^(\w|-|_)+.y*ml")
 
 if [[ "${#allplaybooks[@]}" -eq 0 ]]; then
   echo "No playbooks changed"
   exit 0
 else
-  echo "Playbooks to lint: ${playbooks[*]}"
+  echo "Playbooks to lint: ${allplaybooks[*]}"
 fi
-
 
 echo "Put vault key"
 echo "$ANSIBLE_VAULT_PASSWORD" > ~/.vault_pass.txt
