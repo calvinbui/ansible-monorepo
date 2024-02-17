4d00b9457895df68739b728cfced413dbca4849f
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2024-02-18 00:52:38 +1100

    coturn: remove chmod

--- files/acme.sh/coturn.sh
+++ files/acme.sh/coturn.sh
@@ -8,6 +8,4 @@ KEY=$2
 cp "$CERT" "{{ common_directory }}/coturn/turn_server_cert.pem"
 cp "$KEY" "{{ common_directory }}/coturn/turn_server_pkey.pem"
 
-chmod 744 "{{ common_directory }}/coturn/*.pem"
-
 docker restart coturn
